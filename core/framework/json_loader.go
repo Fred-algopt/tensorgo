f194e9144941195c6c1363a1053626e7f58d296d
Fred-algopt <algopt.fr@gmail.com>
2025-07-21 11:42:04 +0200

    Update json_loader.go

--- core/framework/json_loader.go
+++ core/framework/json_loader.go
@@ -1,1 +1,24 @@
-t
+
+package dataset
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// LoadJSON charge un fichier JSON contenant un tableau d'objets.
+func LoadJSON(filepath string) (*Dataset[map[string]interface{}], error) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lecture fichier JSON : %v", err)
+	}
+
+	var records []map[string]interface{}
+	err = json.Unmarshal(data, &records)
+	if err != nil {
+		return nil, fmt.Errorf("erreur parsing JSON : %v", err)
+	}
+
+	return New(records), nil
+}
