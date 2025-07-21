b62b6f82aacc346af86930c19e23daa510367443
Fred-algopt <algopt.fr@gmail.com>
2025-07-21 11:40:43 +0200

    Update csv_loader.go

--- core/framework/csv_loader.go
+++ core/framework/csv_loader.go
@@ -1,1 +1,49 @@
-t
+package dataset
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+// LoadCSV charge un CSV en slice de map[string]interface{}
+func LoadCSV(filepath string) (*Dataset[map[string]interface{}], error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("erreur ouverture CSV: %v", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	headers, err := reader.Read()
+	if err != nil {
+		return nil, fmt.Errorf("lecture headers: %v", err)
+	}
+
+	var data []map[string]interface{}
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("erreur lecture ligne: %v", err)
+		}
+
+		row := make(map[string]interface{})
+		for i, val := range record {
+			// Tentative de conversion automatique en int ou float
+			if i < len(headers) {
+				if f, err := strconv.ParseFloat(val, 64); err == nil {
+					row[headers[i]] = f
+				} else {
+					row[headers[i]] = val
+				}
+			}
+		}
+		data = append(data, row)
+	}
+	return New(data), nil
+}
